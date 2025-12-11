3ae3636f2c7e0425727737174518c40012d72db1
m-danish77 <mmunibdanish456@gmail.com>
2025-12-11 20:01:40 +0500

    More Notes added

--- Notes.js
+++ Notes.js
@@ -8,9 +8,19 @@ let student = {
 };
 
 let student2 = JSON.parse(JSON.stringify(student)); // Deep Copy
-
 console.log(student === student2); // false both have different refrence
 
 // let student2 = student; // same refrence gives true when comparing
 // same refrence is because we are making copy of refrence not the object
 
+let array1 = [1, 2, 3, null, true, { myName: 'Danish' }];
+console.log(array1); // typeof Array is Object
+
+// We also compare refrences in arrays
+
+// spread operator method is used to make copy of arrays and Objects.
+// but for objects it makes shallow copy, not deep copy
+
+let array2 = [...array1];
+console.log(array2);
+console.log(array1 === array2);
