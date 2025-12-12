e25f34f7012ad177b35b1b5e03b0494a2b6aab36
m-danish77 <mmunibdanish456@gmail.com>
2025-12-12 22:47:49 +0500

    practice2.js created

--- 4 - Routing Requests.js
+++ 4 - Routing Requests.js
@@ -60,13 +60,16 @@ server.listen(PORT, () => {
 //   res.setHeader('Content-type', 'text/html');
 //   let content = '';
 //   let title = '';
-//   if (req.url === '/home') {
+//   if (req.url === '/home' || req.url === '/') {
+//     res.statusCode = 200;
 //     content = `<center><h1>Welcome To Home</h1></center>`;
 //     title = 'Home Page';
 //   } else if (req.url === '/products') {
+//     res.statusCode = 200;
 //     content = `<center><h1>Products are below</h1></center>`;
 //     title = 'Products Page';
 //   } else {
+//     res.statusCode = 404;
 //     content = `<center><h1>Error Code 404</h1></center>`;
 //     title = '404';
 //   }
